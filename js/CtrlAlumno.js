3deaf1877b3f2d2dce012bfbf5ed159dc0185b56
parcialhhm <119424438+parcialhhm@users.noreply.github.com>
2022-11-29 07:41:37 -0600

    Create CtrlAlumno.js

---
+++ js/CtrlAlumno.js
@@ -0,0 +1,118 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraAlumnos
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+const daoAlumno =
+  getFirestore().
+    collection("Alumno");
+const params =
+  new URL(location.href).
+    searchParams;
+const id = params.get("id");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    busca();
+  }
+}
+
+/** Busca y muestra los datos que
+ * corresponden al id recibido. */
+async function busca() {
+  try {
+    const doc =
+      await daoAlumno.
+        doc(id).
+        get();
+    if (doc.exists) {
+      /**
+       * @type {
+          import("./tipos.js").
+                  Alumno} */
+      const data = doc.data();
+      forma.matricula.value = data.matricula;
+      forma.nombre.value = data.nombre || "";
+      forma.telefono.value = data.telefono || "";
+      forma.grupo.value = data.grupo || "";
+      forma.fecha.value = data.fecha || "";
+      forma.addEventListener(
+        "submit", guarda);
+      forma.eliminar.
+        addEventListener(
+          "click", elimina);
+    } else {
+      throw new Error(
+        "No se encontró.");
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraAlumnos();
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  try {
+    evt.preventDefault();
+    const formData =
+      new FormData(forma);
+    const matricula = getString(
+        formData, "matricula").trim();  
+    const nombre = getString(formData, "nombre").trim();
+    const telefono = getString(formData, "telefono").trim();
+    const grupo = getString(formData, "grupo").trim();
+    const fecha = getString(formData, "fecha").trim();
+    /**
+     * @type {
+        import("./tipos.js").
+                Alumno} */
+    const modelo = {
+      matricula, 
+      nombre,
+      telefono,
+      grupo,
+      fecha
+    };
+    await daoAlumno.
+      doc(id).
+      set(modelo);
+    muestraAlumnos();
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " +
+      "eliminación")) {
+      await daoAlumno.
+        doc(id).
+        delete();
+      muestraAlumnos();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
