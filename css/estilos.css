2bf9a91e51c811abe95d72c0b4f1b5e9c9ad3a0e
parcialhhm <119424438+parcialhhm@users.noreply.github.com>
2022-11-29 07:39:39 -0600

    Create estilos.css

---
+++ css/estilos.css
@@ -0,0 +1,62 @@
+@import "mi-nav.css";
+@import "mi-footer.css";
+@import "listas.css";
+
+:root {
+  font-family: sans-serif;
+  --col-active: #4646462c;
+  --color-header: white;
+  --colorMiNav: white;
+}
+
+body {
+  max-width: 640px;
+  margin: 0 auto;
+  padding: 0.5rem;
+}
+
+header {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  background-color:
+    var(--color-header);
+}
+
+.herramientas {
+  display: flex;
+  flex-flow: wrap;
+  align-items: center;
+  align-content: flex-end;
+  justify-content: flex-end;
+  margin-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  gap: 0.5rem;
+}
+
+header input {
+  flex-grow: 1;
+}
+
+h1 {
+  margin: 0.5rem 0;
+}
+
+figure img {
+  max-width: 100%;
+}
+
+legend,
+p label {
+  display: block;
+  font-weight: bold;
+  font-size: 0.7rem;
+}
+
+p label>* {
+  display: block;
+  font-weight: normal;
+  font-size: 1rem;
+  box-sizing: border-box;
+  width: 100%;
+}
